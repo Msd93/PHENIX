5395418bb54f3360acd2ec11ac692bc156f46083
Mamadou seydou DIALLO <diallodiery301gmail.com>
2021-09-19 12:48:43 +0000

    favorite panier ajouté

--- src/app/modules/media/photo/photo-routing.module.ts
+++ src/app/modules/media/photo/photo-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchPhotoComponent } from './search-photo/search-photo.component';
 import { DetailsPictureComponent } from './details-picture/details-picture.component';
 import { UploadPictureComponent } from './upload-picture/upload-picture.component';
+import { FavoritePictureComponent } from './favorite-picture/favorite-picture.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'upload-picture',
     component: UploadPictureComponent
+  },
+  {
+    path: 'favorite-picture',
+    component: FavoritePictureComponent
   }
 ];
 
