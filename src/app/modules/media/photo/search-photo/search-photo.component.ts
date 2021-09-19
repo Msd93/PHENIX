d982bacf88c354c60002d630428bde41af2e1b2a
Alioune Bada Ndoye <abadaa9518@gmail.com>
2021-09-19 16:33:07 +0000

    maj hover on search image

--- src/app/modules/media/photo/search-photo/search-photo.component.ts
+++ src/app/modules/media/photo/search-photo/search-photo.component.ts
@@ -1,22 +1,22 @@
 import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ColorPickerControl, ColorsTable } from '@iplab/ngx-color-picker';
 const IMG: any = [
-  {img: 'coffee-beans-6603499_1280.jpg'},
-  {img: 'rabbits-4890861_1280.jpg'},
-  {img: 'coffee-beans-6603499_1280.jpg'},
-  {img: 'street-6099209_1920.jpg'},
-  {img: 'yellow-macaw-6607739_640.jpg'},
-  {img: 'waterfalls-6476336_1280.jpg'},
-  {img: 'rabbits-4890861_1280.jpg'},
-  {img: 'bird-6602049_1280.jpg'},
-  {img: 'waterfalls-6476336_1280.jpg'},
-  {img: 'yellow-macaw-6607739_640.jpg'},
-  {img: 'street-6099209_1920.jpg'},
-  {img: 'street-6099209_1920.jpg'},
-  {img: 'waterfalls-6476336_1280.jpg'},
-  {img: 'yellow-macaw-6607739_640.jpg'},
-  {img: 'street-6099209_1920.jpg'},
-  {img: 'yellow-macaw-6607739_640.jpg'},
+  {img: 'coffee-beans-6603499_1280.jpg', download: 1215, like: 2500},
+  {img: 'rabbits-4890861_1280.jpg', download: 615, like: 2133},
+  {img: 'coffee-beans-6603499_1280.jpg', download: 435, like: 1243},
+  {img: 'street-6099209_1920.jpg', download: 123, like: 8000},
+  {img: 'yellow-macaw-6607739_640.jpg', download: 897, like: 867},
+  {img: 'waterfalls-6476336_1280.jpg', download: 15, like: 55},
+  {img: 'rabbits-4890861_1280.jpg', download: 125, like: 213},
+  {img: 'bird-6602049_1280.jpg', download: 150, like: 233},
+  {img: 'waterfalls-6476336_1280.jpg', download: 1655, like: 433},
+  {img: 'yellow-macaw-6607739_640.jpg', download: 15, like: 765},
+  {img: 'street-6099209_1920.jpg', download: 45, like: 765},
+  {img: 'street-6099209_1920.jpg', download: 15, like: 934},
+  {img: 'waterfalls-6476336_1280.jpg', download: 15, like: 321},
+  {img: 'yellow-macaw-6607739_640.jpg', download: 8500, like: 10000},
+  {img: 'street-6099209_1920.jpg', download: 435, like: 653},
+  {img: 'yellow-macaw-6607739_640.jpg', download: 678, like: 764},
 ];
 
 @Component({
