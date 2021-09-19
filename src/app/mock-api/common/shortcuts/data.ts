f4114a2bb4356d5b24bd72c9fc38099d1509ba01
smagaye <sma.gaye94@gmail.com>
2021-09-19 01:14:26 +0000

    Shortcut Count

--- src/app/mock-api/common/shortcuts/data.ts
+++ src/app/mock-api/common/shortcuts/data.ts
@@ -1,6 +1,14 @@
 /* eslint-disable */
 export const shortcuts = [
     {
+        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548a',
+        label      : 'Tableau de bord',
+        description: 'Statistiques Globales',
+        icon       : 'heroicons_outline:cog',
+        link       : '/dashboards/global',
+        useRouter  : true
+    },
+    {
         id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548b',
         label      : 'Chargement fichier',
         description: 'Chargement fichier Drag and Drop',
@@ -9,7 +17,7 @@ export const shortcuts = [
         useRouter  : true
     },
     {
-        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548b',
+        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548c',
         label      : 'Details Image',
         description: 'Prévisuation d\'une image et celles similaires',
         icon       : 'heroicons_outline:cog',
@@ -18,7 +26,7 @@ export const shortcuts = [
     },
 
     {
-        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548b',
+        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548d',
         label      : 'Votre Commande',
         description: 'liste des elements commandés',
         icon       : 'heroicons_outline:clipboard-list',
@@ -26,35 +34,35 @@ export const shortcuts = [
         useRouter  : true
     },
     {
-        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548b',
+        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548e',
         label      : 'Rechercher des images',
         description: 'Espace de recherche des image',
         icon       : 'heroicons_outline:search',
         link       : '/media/photo/search-photo',
         useRouter  : true
     },
-    // {
-    //     id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548b',
-    //     label      : 'Parametrage',
-    //     description: 'Espace de configuration du compte',
-    //     icon       : 'heroicons_outline:cog',
-    //     link       : '/pages/settings',
-    //     useRouter  : true
-    // },
     {
-        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548b',
+        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548v',
         label      : 'Home',
         description: 'Ecran D\'accueil',
-        icon       : 'heroicons_outline:search',
+        icon       : 'heroicons_outline:home',
         link       : '/home-phenix/home-phenix',
         useRouter  : true
     },
     {
-        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548b',
+        id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548y',
         label      : 'Contact Us',
         description: 'Contacter le Service',
         icon       : 'heroicons_outline:user',
         link       : '/home-phenix/contact-phenix',
         useRouter  : true
     },
+    // {
+    //     id         : 'a1ae91d3-e2cb-459b-9be9-a184694f548z',
+    //     label      : 'Tableau de bord',
+    //     description: 'Statistiques Globales',
+    //     icon       : 'heroicons_outline:cog',
+    //     link       : '/dashboards/global',
+    //     useRouter  : true
+    // },
 ];
